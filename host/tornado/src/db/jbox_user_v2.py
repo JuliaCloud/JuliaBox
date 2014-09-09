4bc3415ddf0cebecfe51c7fb8db5ec4a2ff990ea
tan <tanmaykm@gmail.com>
2014-09-09 16:06:19 +0530

    fix user_v2

--- host/tornado/src/db/jbox_user_v2.py
+++ host/tornado/src/db/jbox_user_v2.py
@@ -4,6 +4,26 @@ from jbox_util import log_info, CloudHelper
 from jbox_crypto import encrypt, decrypt
 
 class JBoxUserV2():
+    """
+        - user_id (primary hash key)
+        
+        - create_month (global secondary hash key)
+        - create_time (global secondary range index)
+        
+        - update_month  (global secondary hash key)
+        - update_time (global secondary index)
+        
+        - activation_code (global secondary hash key)
+        - activation_status (global secondary range key)
+        
+        - image (optional: global secondary hash key)
+        - resource_profile (optional: global secondary range key)
+        
+        - status
+        - organization
+        - system_profile
+        - gtok
+    """
     NAME = None
     CONN = None
     TABLE = None
@@ -24,7 +44,7 @@ class JBoxUserV2():
             return
         
         try:
-            self.item = JBoxUserV2.TABLE.get_item(user_id)
+            self.item = JBoxUserV2.TABLE.get_item(hash_key=user_id)
             self.is_new = False
         except boto.dynamodb.exceptions.DynamoDBKeyNotFoundError as nfe:
             if create:
@@ -35,27 +55,42 @@ class JBoxUserV2():
                 raise
 
     def get_user_id(self):
+        if None == JBoxUserV2.TABLE:
+            return None
         return self.item['user_id']
 
     def get_status(self):
+        if None == JBoxUserV2.TABLE:
+            return None
         return self.item.get('status', JBoxUserV2.STATUS_ACTIVE)
     
     def set_status(self, status):
+        if None == JBoxUserV2.TABLE:
+            return
         self.item['status'] = status
 
     def set_time(self, prefix, dt=None):
+        if None == JBoxUserV2.TABLE:
+            return
         if None == dt:
             dt = datetime.datetime.now(pytz.utc)
     
         if prefix not in ["create", "update"]:
             raise(Exception("invalid prefix for setting time"))
         
-        self.item[prefix + "_date"] = JBoxUserV2.datetime_to_yyyymmdd(dt)
+        self.item[prefix + "_date"] = JBoxUserV2.datetime_to_yyyymm(dt)
         self.item[prefix + "_time"] = JBoxUserV2.datetime_to_epoch_secs(dt)
     
+    def get_time(self, prefix):
+        if None == JBoxUserV2.TABLE:
+            return None
+        if prefix not in ["create", "update"]:
+            raise(Exception("invalid prefix for setting time"))
+        return JBoxUserV2.epoch_secs_to_datetime(self.item[prefix + "_time"])
+    
     @staticmethod
-    def datetime_to_yyyymmdd(dt):
-        return str(dt.year*10000 + dt.month*100 + dt.day)
+    def datetime_to_yyyymm(dt):
+        return dt.year*100 + dt.month
     
     @staticmethod
     def datetime_to_epoch_secs(dt):
@@ -85,6 +120,8 @@ class JBoxUserV2():
         self.item['activation_status'] = activation_status
     
     def get_activation_state(self):
+        if None == JBoxUserV2.TABLE:
+            return None
         if JBoxUserV2.TABLE is None:
             return (None, None)
         return (self.item.get('activation_code', None), self.item.get('activation_status', JBoxUserV2.ACTIVATION_NONE))
@@ -104,11 +141,13 @@ class JBoxUserV2():
 
     def set_container_type(self, image, resource_profile):
         if None == JBoxUserV2.TABLE:
-            return None
+            return
         self.item['image'] = image
         self.item['resource_profile'] = resource_profile
     
     def get_container_type(self):
+        if None == JBoxUserV2.TABLE:
+            return None
         if JBoxUserV2.TABLE is None:
             return (None, None)
         return (self.item.get('image', None), self.item.get('resource_profile', JBoxUserV2.RESOURCE_PROFILE_BASIC))
