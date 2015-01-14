3133470a69114c47c3eeda030c81568bb9894b1d
tan <tanmaykm@gmail.com>
2015-01-14 12:45:58 +0530

    added Quandl package. fixes #189

--- docker/setup_julia.sh
+++ docker/setup_julia.sh
@@ -14,4 +14,4 @@ julia -e 'Pkg.add("BayesNets"); Pkg.add("PGFPlots"); Pkg.add("GraphLayout");'
 
 julia -e 'Pkg.add("Stan");'
 
-julia -e 'Pkg.add("Patchwork");'
+julia -e 'Pkg.add("Patchwork"); Pkg.add("Quandl");'
