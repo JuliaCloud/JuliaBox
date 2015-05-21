19e671e9ba03c65e3c03db02c5d598ee733baf88
tan <tanmaykm@gmail.com>
2015-05-21 14:37:51 +0530

    Revert "make JuliaBox and cluster work for Julia 0.4"
    This reverts commit c9baedbf5c0f32cd027ca5922a7bbddef378546f.

--- docker/mkjimg.jl
+++ docker/mkjimg.jl
@@ -1,4 +1,4 @@
-require("/opt/julia/share/julia/build_sysimg.jl")
+require("/home/juser/build_sysimg.jl")
 
 println("Building JuliaBox Julia system image...")
 build_sysimg("/home/juser/.juliabox/jimg/sys", "native", "/home/juser/jimg.jl", force=true)
