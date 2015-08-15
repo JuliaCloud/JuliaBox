6a2928be3710621b549169f3ca72c84d4740af16
Samuel Powell <samuelpowell@users.noreply.github.com>
2015-08-15 18:17:23 +0100

    Make import of Gadfly conditional

--- docker/jimg.jl
+++ docker/jimg.jl
@@ -1,2 +1,4 @@
-include(joinpath(Pkg.dir("Gadfly"), "src/Gadfly.jl"))
-importall Gadfly
+if Pkg.installed("Gadfly") != nothing
+	include(joinpath(Pkg.dir("Gadfly"), "src/Gadfly.jl"))
+	importall Gadfly
+end
